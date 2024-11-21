ec0d3b0eabdfc21c90dc086ce3ad6a9c188255ff
Jovan Tanasijevic <jocatanas47@gmail.com>
2024-11-21 18:20:30 +0100

    added password encoder

--- src/main/java/com/jovantanasijevic/flashcard_generator/config/WebSecurityConfig.java
+++ src/main/java/com/jovantanasijevic/flashcard_generator/config/WebSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -32,5 +34,10 @@ public class WebSecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         return new JdbcUserDetailsManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/jovantanasijevic/flashcard_generator/service/RegistrationService.java
+++ src/main/java/com/jovantanasijevic/flashcard_generator/service/RegistrationService.java
@@ -2,10 +2,15 @@ package com.jovantanasijevic.flashcard_generator.service;
 
 import com.jovantanasijevic.flashcard_generator.dto.UserRegistrationRequestDto;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegistrationService {
+    private final JdbcUserDetailsManager jdbcUserDetailsManager;
+    private final PasswordEncoder passwordEncoder;
+
     public void registerUser(UserRegistrationRequestDto userRegistrationRequestDto) {
 
     }
