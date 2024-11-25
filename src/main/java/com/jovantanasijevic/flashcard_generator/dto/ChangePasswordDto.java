b0b1c9bd45124a8d3fad635fdf7de5a54f868787
Jovan Tanasijevic <jocatanas47@gmail.com>
2024-11-25 13:33:08 +0100

    added dto validation

--- src/main/java/com/jovantanasijevic/flashcard_generator/dto/ChangePasswordDto.java
+++ src/main/java/com/jovantanasijevic/flashcard_generator/dto/ChangePasswordDto.java
@@ -1,7 +1,14 @@
 package com.jovantanasijevic.flashcard_generator.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class ChangePasswordDto {
+    @NotBlank(message = "old password is required")
     private String oldPassword;
+    @NotBlank(message = "new password is required")
+    @Size(min = 12, message = "password must be at least 12 characters long")
     private String newPassword;
 
     public String getOldPassword() {
--- src/main/java/com/jovantanasijevic/flashcard_generator/dto/UserRegistrationRequestDto.java
+++ src/main/java/com/jovantanasijevic/flashcard_generator/dto/UserRegistrationRequestDto.java
@@ -1,8 +1,18 @@
 package com.jovantanasijevic.flashcard_generator.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class UserRegistrationRequestDto {
+    @NotNull(message = "username is required")
+    @Size(min = 3, max = 32, message = "username must be between 3 and 32 characters long")
     private String username;
+    @NotNull(message = "password is required")
+    @Size(min = 12, message = "password must be at least 12 characters long")
     private String password;
+    @NotNull(message = "email is required")
+    @Email(message = "invalid email address")
     private String email;
 
     public UserRegistrationRequestDto(String username, String password, String email) {
