b947a921ce2b20da36748bb0c287ea82f2b13067
Jovan Tanasijevic <jocatanas47@gmail.com>
2024-11-25 13:16:35 +0100

    added constructors and getters and setters to Word.java

--- src/main/java/com/jovantanasijevic/flashcard_generator/domain/Word.java
+++ src/main/java/com/jovantanasijevic/flashcard_generator/domain/Word.java
@@ -11,4 +11,27 @@ public class Word {
 
     @Column(nullable = false, unique = true)
     private String word;
+
+    public Word() {
+    }
+
+    public Word(String word) {
+        this.word = word;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
 }
