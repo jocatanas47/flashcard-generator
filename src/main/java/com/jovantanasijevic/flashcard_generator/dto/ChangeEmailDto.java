03650f01e8a9b9cbb5b093a0914909182e655277
Jovan Tanasijevic <jocatanas47@gmail.com>
2024-11-25 13:24:59 +0100

    added ChangeEmailDto validation

--- src/main/java/com/jovantanasijevic/flashcard_generator/dto/ChangeEmailDto.java
+++ src/main/java/com/jovantanasijevic/flashcard_generator/dto/ChangeEmailDto.java
@@ -1,6 +1,9 @@
 package com.jovantanasijevic.flashcard_generator.dto;
 
+import jakarta.validation.constraints.Email;
+
 public class ChangeEmailDto {
+    @Email
     private String email;
 
     public String getEmail() {
