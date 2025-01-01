dcd10e5c31241dd2fcf496b8bbfc28171b14baea
Jovan Tanasijevic <jocatanas47@gmail.com>
2025-01-02 00:21:55 +0100

    added FlashcardGeneratorApplication.java to git

---
+++ src/main/java/com/jovantanasijevic/flashcard_generator/FlashcardGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.jovantanasijevic.flashcard_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardGeneratorApplication.class, args);
+	}
+
+}
